9e3209d16fc201ae9812ffc81aea1b0744b398ff
hari krishna <86965406+Githubhari@users.noreply.github.com>
2024-01-11 12:29:39 +0530

    Update forms.py

--- wordCount/wordcounter/forms.py
+++ wordCount/wordcounter/forms.py
@@ -4,7 +4,7 @@ from django.core import validators
 class TextForm(forms.Form):
 
     sentence = forms.CharField(widget=forms.Textarea(attrs={'oninput':'countWords()',
-                                                            'style':'min-height:350px;margin-bottom:10px',
+                                                            'style':'min-height:10vw;margin-bottom:10px',
                                                             'class':'form-control','placeholder':'Please Enter text here...'}))
     # WordCount = forms.BooleanField(required=False)
     # LineCount = forms.BooleanField(required=False)
